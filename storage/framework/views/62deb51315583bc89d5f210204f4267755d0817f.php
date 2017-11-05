7f87b00085717a20035265bf8dae0a9fcfeedb2d
Maxime <maxime.lutz@gmail.com>
2017-11-05 23:47:51 +0100

    0.5.2 - maj flexbox

--- storage/framework/views/62deb51315583bc89d5f210204f4267755d0817f.php
+++ storage/framework/views/62deb51315583bc89d5f210204f4267755d0817f.php
@@ -5,27 +5,31 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dernière étape</div>
                 <div class="panel-body">
-                    <?php if(!$user->hasCotisation()): ?>
-                        <p>Pour finaliser votre adhésion il vous reste à nous faire parvenir votre cotisation, de préférence par virement :</p>
-                        <img src="<?php echo e(url('/images/administratif/rib_fourmiliere.png')); ?>">
-                        <p>Sinon par chèque à Hélène HENRY,<br />
-                        18 rue Champs Verts,<br />
-                        67880 KRAUTERGERSHEIM</p>
-                        <p>Pour l’année 2017, la cotisation a été fixé à 15€ par foyer.<br />
-                        Si vous le souhaitez, vous pouvez augmenter ce montant pour soutenir nos actions et faciliter le développement de l’association.</p>
 
-                        <p>Fourmilièrement vôtre,</p>
-                        <p>Insertion logo (comme une carte de visite en carte de visite)</p>
-                        <p>PS: un mail vous a été envoyé avec nos coordonnées bancaire et postale.</p>
+                    <?php if(!$user->hasCotisation()): ?>
+                        <p>Pour l’année 2017, la cotisation a été fixé à 15€ par foyer.
+                        Si vous le souhaitez, vous pouvez augmenter ce montant pour soutenir nos actions et faciliter le développement de l’association.</p>
+                        <p>Afin de finaliser votre adhésion, il ne vous reste plus qu'à nous faire parvenir votre cotisation, de préférence par virement :</p>
+                        <p style="text-align: center;"><img src="<?php echo e(url('/images/administratif/rib_fourmiliere.png')); ?>"></p><br><br>
+                        <p>Ou par chèque à :<br>
+                        Hélène SCHERMESSER<br>
+                        18 rue Champs Verts<br>
+                        67880 KRAUTERGERSHEIM</p>
+                        <p>Vous recevrez aussi un mail avec nos coordonnées bancaires et postales.</p>
+                        <br>
+                        <p>La cotisation est valable pour 2 membres d'un même foyer. Vous pouvez nous indiquer quel membre rajouter à votre cotisation en cliquant sur le bouton ci-dessous et en renseignant les noms et prénoms de la personne concernée.</p>
+                        <p><a href="<?php echo e(url('/account/addpartner')); ?>"  class="btn btn-primary">Ajouter un partenaire</a></p>
                     <?php endif; ?>
+
                     <?php if(!$user->hasvalidemail()): ?>
-                        <p>Vous n'avez pas encore validé votre mail.<br />
-                        Pour cela, merci de cliquer sur le lien  présent dans le mail que nous vous avons envoyé.<br />
-                        Si vous n'arrivez pas à valider votre mail, merci de nous contacter à l'adresse : contact@laforumilière.org</p>
+                    <br><br>
+                            <p>Vous n'avez pas encore validé votre mail.<br />
+                            Pour cela, merci de cliquer sur le lien  présent dans le mail que nous vous avons envoyé.<br />
+                            Si vous n'arrivez pas à valider votre mail, merci de nous contacter à l'adresse : contact@laforumilière.org</p>
                     <?php endif; ?>
-                        <br /><br /><br />
-                        <p>La cotisation est valable pour 2 membres d'une même famille. Vous pouvez nous indiquer à quel membre rajouter votre cotisation à l'aide du bouton ci-dessous. </p>
-                    <a href="<?php echo e(url('/account/addpartner')); ?>"  class="btn btn-primary">Ajouter un partenaire</a>
+                    <br><br>
+                    <p>Fourmilièrement vôtre</p>
+
                 </div>
             </div>
 
